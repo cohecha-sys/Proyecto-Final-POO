2fd98ebfb2846c34f59ac7a87b3e74177bf5a6dd
Juan Leon <juanbg8513@gmail.com>
2026-09-14 14:49:02 -0400

    P1 creacion

---
+++ src/ejercicio_biblioteca.java
@@ -0,0 +1,10 @@
+public class ejercicio_biblioteca {
+
+package com.biblioteca;
+
+    public class Libro {
+        private String titulo;
+        private String autor;
+        private String isbn;
+    }
+}
